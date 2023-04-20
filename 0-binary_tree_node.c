d37be678b08151c9e81ba959df2213e32483bdb3
adel <6126.holbertonstudents.com>
2023-04-20 16:11:26 -0700

    DOS line Ending error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,18 +7,16 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *knew_knode;
+binary_tree_t *knew_knode;
 
-    knew_knode = malloc(sizeof(binary_tree_t));
+knew_knode = malloc(sizeof(binary_tree_t));
 
+if (knew_knode == NULL)
+return (NULL);
+knew_knode->n = value;
+knew_knode->parent = parent;
+knew_knode->left = NULL;
+knew_knode->right = NULL;
 
-    if (knew_knode == NULL)
-    return (NULL);
-
-    knew_knode->n = value;
-    knew_knode->parent = parent;
-    knew_knode->left = NULL;
-    knew_knode->right = NULL;
-
-    return(knew_knode);
+return (knew_knode);
 }
