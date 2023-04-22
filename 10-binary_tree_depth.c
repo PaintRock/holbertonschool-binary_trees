6ce39e209c4c0a6183ad71c44ccd2bbbfffc095d
adel <6126.holbertonstudents.com>
2023-04-22 10:17:09 -0700

    depth should be width

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,24 +7,13 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-int right_depth = 0;
-int left_depth = 0;
 
 if (!tree)
 return (0);
 
-if (!tree->left && !tree->right)
+if (!tree->parent)
 return (0);
 
-left_depth = binary_tree_depth(tree->left);
-right_depth = binary_tree_depth(tree->right);
-/* return the larger of the two depths, plus one for the current node */
-if (left_depth > right_depth)
-{
-return (left_depth + 1);
-}
-else
-{
-return (right_depth + 1);
-}
+return (binary_tree_depth(tree->parent) + 1);
+
 }
