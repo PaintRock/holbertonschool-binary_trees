837892628266cc5b88a22b665f9e7701a8af54cc
adel <6126.holbertonstudents.com>
2023-04-22 10:46:08 -0700

    took out if does not exit return NULL

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,8 +12,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 if (!tree)
 return (0);
 
-if (!tree->parent)
-return (0);
 
 biggie = (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 return (biggie);
