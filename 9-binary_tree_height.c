5c79d16772d02cb3131f0d1392b7dbb4c6be068d
adel <6126.holbertonstudents.com>
2023-04-21 17:00:12 -0700

    three of seven

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,15 +13,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+        if (!tree->left && !tree->right)
+        return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 /* return the larger of the two heights, plus one for the current node */
 if (left_height > right_height)
 {
-return (left_height + 1);
+return (left_height);
 }
 else
 {
-return (right_height + 1);
+return (right_height);
 }
 }
